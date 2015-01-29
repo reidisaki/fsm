0ccb7fc96e4b0fc646f996e6c89cfd5415273e60
Reid Isaki <risaki@revision3.com>
2015-01-29 02:33:22 -0800

    placeholder image caching

--- src/com/example/fullscreen_reidisaki_test/data/IGdataAdapter.java
+++ src/com/example/fullscreen_reidisaki_test/data/IGdataAdapter.java
@@ -5,11 +5,13 @@ import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.List;
 
-import android.app.Activity;
+import android.app.ActivityManager;
 import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.drawable.BitmapDrawable;
 import android.os.AsyncTask;
+import android.support.v4.util.LruCache;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -21,7 +23,7 @@ import android.widget.TextView;
 import com.example.fullscreen_reidisaki_test.R;
 
 public class IGdataAdapter extends ArrayAdapter<InstagramData> {
-
+	private LruCache<String, Bitmap> mMemoryCache;
 	int layoutResourceId;
 	Context mContext;
 	List<InstagramData> data;
@@ -30,7 +32,29 @@ public class IGdataAdapter extends ArrayAdapter<InstagramData> {
 		this.layoutResourceId = igRowId;
 		this.mContext = context;
 		this.data = objects;
-		Log.i("reid",objects.size() +" size");
+		
+		int memClass = ( ( ActivityManager )context.getSystemService( Context.ACTIVITY_SERVICE ) ).getMemoryClass();
+		int cacheSize = 1024 * 1024 * memClass / 8;
+		
+		mMemoryCache = new LruCache<String, Bitmap>(cacheSize) {
+			@Override
+			protected int sizeOf(String key, Bitmap bitmap) {
+				// The cache size will be measured in kilobytes rather than
+				// number of items.
+				return bitmap.getByteCount() / 1024;
+			}
+		};
+	}
+	public void addBitmapToMemoryCache(String key, Bitmap bitmap) {
+		if(key != null && bitmap != null) {
+			if (getBitmapFromMemCache(key) == null) {
+				mMemoryCache.put(key, bitmap);
+			}
+		}
+	}
+
+	public Bitmap getBitmapFromMemCache(String key) {
+		return mMemoryCache.get(key);
 	}
 	@Override
 	public View getView(int position, View convertView, ViewGroup parent) {
@@ -49,47 +73,66 @@ public class IGdataAdapter extends ArrayAdapter<InstagramData> {
 			ImageView image = (ImageView)convertView.findViewById(R.id.ig_image);
 			ImageView thumb = (ImageView)convertView.findViewById(R.id.ig_image_thumb);
 			TextView text = (TextView)convertView.findViewById(R.id.ig_text);
-			Log.i("reid",currentData.get_fullName());
+			//			Log.i("reid",currentData.get_fullName());
 			text.setText(currentData.get_fullName());
-			Log.i("reid",currentData.get_standardImage());
+			//			Log.i("reid",currentData.get_standardImage());
 			image.setTag(currentData.get_standardImage());
 			thumb.setTag(currentData.get_ownerPicture());
-			new DownloadImagesTask().execute(image);
-			new DownloadImagesTask().execute(thumb);
+			Bitmap b = getBitmapFromMemCache(currentData.get_standardImage());
+			if(b== null) {
+				image.setImageResource(R.drawable.download);
+//				image.setImageBitmap(((BitmapDrawable)mContext.getResources().getDrawable(R.drawable.download)).getBitmap());
+				new DownloadImagesTask().execute(image);	
+			} else {
+				Log.i("reid","cache hit");
+				image.setImageBitmap(b);
+			}
+			Bitmap b_thumb = getBitmapFromMemCache(currentData.get_ownerPicture());
+			if(b_thumb == null) {
+				new DownloadImagesTask().execute(thumb);	
+			} else {
+				Log.i("reid","cache hit thumb");
+				thumb.setImageBitmap(b_thumb);
+			}
 		}
-		
+
 		return convertView;
 	}
-	
+
 	public class DownloadImagesTask extends AsyncTask<ImageView, Void, Bitmap> {
 
-	    ImageView imageView = null;
+		ImageView imageView = null;
+		String _url;
+		@Override
+		protected Bitmap doInBackground(ImageView... imageViews) {
+			
+			this.imageView = imageViews[0];
+			_url = (String)imageView.getTag();
+			Log.i("reid","downloading image " + _url);
+			Bitmap result = download_Image((String)imageView.getTag());
+			addBitmapToMemoryCache(_url,result);
+			return result;
+		}
 
-	    @Override
-	    protected Bitmap doInBackground(ImageView... imageViews) {
-	        this.imageView = imageViews[0];
-	        return download_Image((String)imageView.getTag());
-	    }
+		@Override
+		protected void onPostExecute(Bitmap result) {
+			imageView.setImageBitmap(result);
+		}
 
-	    @Override
-	    protected void onPostExecute(Bitmap result) {
-	        imageView.setImageBitmap(result);
-	    }
+		private Bitmap download_Image(String url) {
 
-	    private Bitmap download_Image(String url) {
+			Bitmap bmp =null;
+			try{
+				URL ulrn = new URL(url);
+				HttpURLConnection con = (HttpURLConnection)ulrn.openConnection();
+				InputStream is = con.getInputStream();
+				bmp = BitmapFactory.decodeStream(is);
+				if (null != bmp)
+					return bmp;
 
-	        Bitmap bmp =null;
-	        try{
-	            URL ulrn = new URL(url);
-	            HttpURLConnection con = (HttpURLConnection)ulrn.openConnection();
-	            InputStream is = con.getInputStream();
-	            bmp = BitmapFactory.decodeStream(is);
-	            if (null != bmp)
-	                return bmp;
-
-	            }catch(Exception e){}
-	        return bmp;
-	    }
+			}catch(Exception e){}
+			return bmp;
+		}
 	}
 }
 
